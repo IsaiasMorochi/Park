3a280222b7956ef4c83312372969ac990a0605ba
IsaiasMorochi <isaiasmorochi95@gmail.com>
2018-07-14 12:04:31 -0400

    Agregado de Dependencias

--- app/src/main/java/com/isaias/park/MapsActivity.java
+++ app/src/main/java/com/isaias/park/MapsActivity.java
@@ -27,10 +27,12 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+
 import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
